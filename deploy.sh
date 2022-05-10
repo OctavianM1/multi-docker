4b146f5a1df71ea14c1744ca24d3ddd6902e71ef
Octavian Mitu <octavian.mitu@amdaris.com>
2022-05-10 12:48:36 +0300

    updated images in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 docker build -t octavianmitu/multi-client:latest -t octavianmitu/multi-client:$SHA ./client
 docker build -t octavianmitu/multi-server:latest -t octavianmitu/multi-server:$SHA ./server
 docker build -t octavianmitu/multi-worker:latest -t octavianmitu/multi-worker:$SHA ./worker
-docker push octavianmitu/multi-client:latest octavianmitu/multi-client:$SHA
-docker push octavianmitu/multi-server:latest octavianmitu/multi-server:$SHA
-docker push octavianmitu/multi-worker:latest octavianmitu/multi-worker:$SHA
+
+docker push octavianmitu/multi-client:latest 
+docker push octavianmitu/multi-client:$SHA
+docker push octavianmitu/multi-server:latest 
+docker push octavianmitu/multi-server:$SHA
+docker push octavianmitu/multi-worker:latest 
+docker push octavianmitu/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=octavianmitu/multi-server:$SHA
 kubectl set image deployments/client-deployment client=octavianmitu/multi-client:$SHA
